749825ea3c9ed3443f9fb092d77f44ff8709e9c1
Sean Loper <grimmdev@gmail.com>
2016-05-14 23:21:06 -0400

    Added Image Effects, Input System, Working on Tiger Controller.

---
+++ Unity/Assets/Code/ControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Rewired;
+
+public class ControllerTest : MonoBehaviour {
+
+	public int Speed = 1;
+	private float Normalize = 0.01f;
+	private int playerID = 0;
+	private Player player;
+
+	private Vector3 thisInput = Vector3.zero;
+
+	// Use this for initialization
+	private void Awake ()
+	{
+		player = ReInput.players.GetPlayer (playerID);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		thisInput.x = (player.GetAxis ("Move Horizontal") * Normalize) * Speed;
+		thisInput.z = (player.GetAxis ("Move Vertical") * Normalize) * Speed;
+		transform.Translate (thisInput);
+	}
+}
---
+++ Unity/Assets/Code/Screenshot.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Screenshot : MonoBehaviour {
+
+	public float Scale = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LateUpdate() {
+		Camera Mcamera = Camera.main;
+		if(Input.GetKeyDown(KeyCode.P)) {
+			int rw = (int)Mathf.Round(Mcamera.pixelWidth * Scale);
+			int rh = (int)Mathf.Round(Mcamera.pixelHeight * Scale);
+			RenderTexture rt = new RenderTexture(rw, rh, 24);
+			Mcamera.targetTexture = rt;
+			Texture2D ss = new Texture2D(rw, rh, TextureFormat.RGB24, false);
+			Mcamera.Render();
+			RenderTexture.active = rt;
+			ss.ReadPixels(new Rect(0, 0, rw, rh), 0, 0);
+			Mcamera.targetTexture = null;
+			RenderTexture.active = null;
+			Destroy(rt);
+			byte[] bytes = ss.EncodeToPNG();
+			string filename = "screenshot.png";
+			System.IO.File.WriteAllBytes(filename, bytes);
+			Debug.Log(string.Format("Took screenshot to: {0}", filename));
+		}
+	}
+}
